0ffe425d4ca23a8703affa2fb3079e7713f84e69
Mrkleenn <suparuek2542@hotmail.com>
2022-02-21 19:02:51 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,179 @@
+<html>
+
+<head>
+
+    <title>task5</title>
+    <meta charset="UTF-8">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,300&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,300&display=swap">
+    <link rel="stylesheet" href="css.css">
+
+</head>
+
+<body>
+    <!--start navber-->
+    <header>
+        <nav>
+            <div class="navber container">
+                <h1>
+                    <a href="">URU</a>
+                </h1>
+
+                <h1>
+                    <a href="">Uttaradit Rajabhat University<br>
+                        <a1>Industrial echnology</a1>
+                    </a>
+
+                </h1>
+
+            </div>
+
+        </nav>
+    </header>
+
+
+    <header>
+        <nav>
+            <div class="navber1 container1">
+
+                <ul class="list">
+
+                    <li>
+                        <a href="">Contact Us</a>
+                    </li>
+                    <li>
+                        <a href="">Download</a>
+                    </li>
+                    <li>
+                        <a href="">Journal</a>
+                    </li>
+                    <li>
+                        <a href="">Editor</a>
+                    </li>
+                    <li>
+                        <a href="">About</a>
+                    </li>
+
+                </ul>
+
+            </div>
+        </nav>
+    </header>
+
+    <!--end navber-->
+
+    <!--start section using background-->
+
+    <section class="background">
+        <div>
+            <h1>
+            Technology & Innovation URU Journal
+            </h1>
+            <p>
+         
+            วารสารเทคโนโลยีและนวัตกรรม มหาวิทยาลัยราชภัฏอุตรดิตถ์ ได้จัดทำขึ้นเพื่อเป็นสื่อกลางการเผยแพร่บทความงานวิจัยและผลงานทางวิชาการ <br> 
+            ทางด้านวิทยาศาสตร์และเทคโนโลยี รวมถึงนวัตกรรมต่าง ๆ ที่สร้างสรรค์และเป็นประโยชน์ต่อชุมชน และ อุตสาหกรรมขนาดกลาง  <br>
+            และ ขนาดย่อม โดยทุกบทความผ่านการพิจารณากลั่นกรองจากผู้ทรงคุณวุฒิ (Peer review) จากในสาขาที่เกี่ยวข้อง  <br>
+            จำนวนอย่างน้อย 2 ท่านต่อบทความ โดยกองบรรณาธิการมีสิทธิ์แก้ไขบทความตามความเหมาะสม <br>
+
+            </p>
+
+            <!-- <button>
+                <a href="">Get Started Naw</a>
+            </button> -->
+
+        </div>
+    </section>
+
+    <!--end section using background-->
+
+    <!--start section using about-->
+    <section class="about">
+
+        <div>
+
+            <h1>
+            วารสารเทคโนโลยีและนวัตกรรม มหาวิทยาลัยราชภัฏอุตรดิตถ์
+            </h1>
+
+            <p>
+          
+            หลักการและเหตุผล <br>
+            คณะเทคโนโลยีอุตสาหกรรม มหาวิทยาลัยราชภัฏอุตรดิตถ์ ได้เล็งเห็นถึงความสำคัญในการส่งเสริมและสนับสนุน ผลงานวิจัย งาน <br>
+            สร้างสรรค์ นวัตกรรม วิทยาศาสตร์และเทคโนโลยี ที่มีคุณภาพทั้งในประเทศและต่างประเทศ  ที่นำไปใช้ประโยชน์ให้กับชุมชน <br>
+            และ อุตสาหกรรม เพื่อขับเคลื่อนประเทศไทยสู่ความมั่งคั่ง มั่นคง และ ยั่งยืน <br>
+
+            ดังนั้นเพื่อเป็นการเผยแพร่ผลงานและองค์ความรู้เหล่านี้ในวง วิชาการอย่างกว้างขวาง คณะเทคโนโลยีอุตสาหกรรม มหาวิทยาลัย  <br>ราชภัฏอุตรดิตถ์
+            จึงเห็นสมควรในการจัดทำวารสารเทคโนโลยีและนวัตกรรม มหาวิทยาลัยราชภัฏอุตรดิตถ์ <br>
+            เพื่อเป็นทางเลือกหนึ่งในการเผยแพร่ผลงานวิชาการ <br>
+          
+            วัตถุประสงค์ <br>
+            1. เพื่อเผยแพร่และถ่ายทอดผลงานวิชาการและวิจัย ในรูปของสิ่งตีพิมพ์ <br>
+            2. เพื่อสนับสนุนการนำผลงานวิชาการ และวิจัยไปใช้ให้เกิดประโยชน์ <br>
+            3. เพื่อสร้างเครือข่ายการเผยแพร่ผลงานวิชาการและวิจัย ระหว่างมหาวิทยาลัยกับหน่วยงานภายนอกทั้งภาครัฐและเอกชน <br>
+
+            กำหนดการออกวารสาร <br>
+            วารสารวิชาการเทคโนโลยีและนวัตกรรมมีกำหนดการออกปีละ 2 ฉบับ <br>
+            ฉบับที่ 1 จะจัดพิมพ์ระหว่างเดือนมกราคม และมิถุนายน ของทุกปี <br>
+            ฉบับที่ 2 จะจัดพิมพ์ระหว่างเดือนสิงหาคม และธันวาคม ของทุกปี <br>
+
+
+
+            </p>
+
+        </div>
+
+    </section>
+    <!--end section using about-->
+
+    <!--back section using box-->
+    <footer class="box">
+        <div class=" boxPassion">
+            <i class="fa fa-heart-o fa-3x" style="font-size:40px; color:#84d4d8"></i>
+            <h3>
+                Passion
+            </h3>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, aut quod fugit,
+                maiores in minima itaque repudiandae, tenetur qui impedit veniam voluptatem odio magni '
+                natus delectus molestiae asperiores neque alias!
+            </p>
+        </div>
+        <div class="boxcreative">
+            <i class="fa fa-lightbulb-o fa-3x" style="font-size:40px; color:#84d4d8"></i>
+            <h3>
+                creative
+            </h3>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, aut quod fugit,
+                maiores in minima itaque repudiandae, tenetur qui impedit veniam voluptatem odio magni '
+                natus delectus molestiae asperiores neque alias!
+            </p>
+
+        </div>
+        <div class="boxQuality">
+            <i class="fa fa-star-o fa-3x" style="font-size:40px; color:#84d4d8"></i>
+            <h3>
+                Quality
+
+            </h3>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, aut quod fugit,
+                maiores in minima itaque repudiandae, tenetur qui impedit veniam voluptatem odio magni '
+                natus delectus molestiae asperiores neque alias!
+            </p>
+            <!--start section using box-->
+        </div>
+    </footer>
+    <!--end section using box-->
+</body>
+
+</html>
+
+
+</body>
+
+</html>
